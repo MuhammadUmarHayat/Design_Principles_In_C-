a2abc0f56107503db7a6849d1ae121b7f85ed5d4
Hafiz Muhammad Umar Hayat <52619520+MuhammadUmarHayat@users.noreply.github.com>
2025-03-21 21:31:58 +0500

    Add files via upload

---
+++ RepositoryExmpleWithSQL/CustomerRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Reflection;
+using System.Xml.Linq;
+
+
+namespace RepositoryExmple1
+{
+    internal class CustomerRepository:ICustomer
+    {
+        private string conString = @"";
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Customers", connection);
+                var reader = command.ExecuteReader();
+                var customers = new List<Customer>();
+
+                while (reader.Read())
+                {
+                    customers.Add(new Customer
+                    {
+                        customerId = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Email = reader.GetString(2)
+                    });
+                }
+
+                return customers;
+            }
+        }
+        public Customer GetById(int id)
+        {
+                using (var connection = new SqlConnection(conString))
+                {
+                    connection.Open();
+                    var command = new SqlCommand("SELECT * FROM Customers WHERE CustomerId = @CustomerId", connection);
+                    command.Parameters.AddWithValue("@CustomerId", id);
+                    var reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        return new Customer
+                        {
+                            customerId = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Email = reader.GetString(2)
+                        };
+                    }
+
+                    return null;
+                }
+            }
+          public   Boolean addCustomer(Customer customer)
+        {
+            try {
+                SqlConnection sqlConn = new SqlConnection(conString);
+
+                sqlConn.Open();
+                string query = "insert into customers(name,email) values('"+customer.Name+"','"+customer.Email+"')";
+                SqlCommand cmd = new SqlCommand(query, sqlConn);
+                cmd.ExecuteNonQuery();
+                sqlConn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+
+        }
+       public  Boolean updateCustomer(Customer customer)
+        {
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(conString);
+
+                sqlConn.Open();
+                string query = "";
+                SqlCommand cmd = new SqlCommand(query, sqlConn);
+                cmd.ExecuteNonQuery();
+                sqlConn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+       public  Boolean deleteCustomer(int id)
+        {
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(conString);
+
+                sqlConn.Open();
+                string query = "";
+                SqlCommand cmd = new SqlCommand(query, sqlConn);
+                cmd.ExecuteNonQuery();
+                sqlConn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RepositoryExmpleWithSQL/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RepositoryExmple1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerRepository repository = new CustomerRepository();
+            Customer customer1 = new Customer();
+            customer1.Name = "Amna";
+            customer1.Email = "am@gmail.com";
+
+            repository.addCustomer(customer1);
+
+           var  customers = repository.GetAllCustomers();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"Customer ID: {customer.customerId}, Name: {customer.Name}, Email: {customer.Email}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
